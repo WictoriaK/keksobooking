b827ecafe33a9dc113a76a2513292931365e2c16
Victoria <vitagood.92@list.ru>
2025-09-24 22:12:53 +0300

    finished 2.15

---
+++ js/main.js
@@ -0,0 +1,27 @@
+//Функция, возвращающая случайное целое число из переданного диапазона включительно.
+
+const getRandomPositiveInteger = (a, b = 1) => {
+  if (a === undefined) {
+    throw new Error('Первый параметр должен быть число');
+  }
+
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+// Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
+const getRandomFloatNumber = (a, b, digits = 1) => {
+  if (a === undefined) {
+    throw new Error('Первый параметр должен быть число');
+  }
+
+  const lower = Math.min(Math.abs(a), Math.abs(b));
+  const upper = Math.max(Math.abs(a), Math.abs(b));
+  const result = Math.random() * (upper - lower) + lower;
+  return +result.toFixed(digits);
+}
+
+getRandomFloatNumber(-1, 2, 1);
+getRandomPositiveInteger(1,1)
