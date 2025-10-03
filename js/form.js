571a4a30565868793c52a3f053b5507adfb0b64f
Victoria <vitagood.92@list.ru>
2025-10-03 21:58:39 +0300

    finished module-7.11

---
+++ js/form.js
@@ -0,0 +1,40 @@
+const advertForm = document.querySelector('.ad-form');
+const advertFormFields = advertForm.querySelectorAll('fieldset');
+
+const mapFormFilters = document.querySelector('.map__filters');
+const mapFormFields = mapFormFilters.querySelectorAll('.map__filter');
+const mapFormFeatures = mapFormFilters.querySelector('.map__features');
+
+
+const setUnactiveFormFieldsState = (formFields) => {
+  formFields.forEach(field => {
+    field.disabled = true;
+  });
+};
+
+const setUnactiveFormState = () => {
+  advertForm.classList.add('.ad-form--disabled');
+  mapFormFilters.classList.add('.ad-form--disabled');
+
+  setUnactiveFormFieldsState(advertFormFields);
+  setUnactiveFormFieldsState(mapFormFields);
+  mapFormFeatures.disabled = true;
+};
+
+const setActiveFormFieldsState = (formFields) => {
+  formFields.forEach(field => {
+    field.disabled = false;
+  });
+};
+
+const setActiveFormState = () => {
+  advertForm.classList.remove('.ad-form--disabled');
+  mapFormFilters.classList.remove('.ad-form--disabled');
+
+  setActiveFormFieldsState(advertFormFields);
+  setActiveFormFieldsState(mapFormFields);
+  mapFormFeatures.disabled = false;
+};
+
+
+setUnactiveFormState();
